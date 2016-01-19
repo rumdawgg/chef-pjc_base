abdf09076e5a941aa81ee5457df5eebe4d2a6990
Paul Chicarello <pchicarello@patientslikeme.com>
2016-01-19 14:44:47 -0500

    MOTD recipe FINALLY works

--- recipes/motd.rb
+++ recipes/motd.rb
@@ -4,15 +4,11 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package "figlet" do
-    action :install
+chef_gem "artii" do
+	action :install
 end
 
-chef_gem "figlet"
-require "figlet"
-
-font = Figlet::Font.new('big')
-figlet = Figlet::Typesetter.new(font)
+require "artii"
 
 template "/etc/motd" do
     source "motd.erb"
