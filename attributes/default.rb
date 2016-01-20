11782f1013a3f85bd972ef8ab9b4da13559dca7a
Paul Chicarello <pchicarello@patientslikeme.com>
2016-01-20 15:03:39 -0500

    set use PAM to no. otherwise you get two MOTDs

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,4 +11,19 @@ default.openssh.server.permit_root_login = 'without-password'
 default.openssh.server.password_authentication = 'no'  
 # default.openssh.server.allow_groups = 'sudo'  
 # default.openssh.server.login_grace_time = '30'  
-# default.openssh.server.use_p_a_m = 'no'
+default.openssh.server.use_p_a_m = 'no'
+# The Mirage is a 3,044 room hotel and casino resort located on the Las Vegas
+# Strip in Paradise, Nevada. The marquee in front of the Mirage is the largest
+# free standing marquee in the world. The Mirage was built by developer Steve
+# Wynn and designed by Joel Bergman. It opened in November 1989 and was the first
+# resort that was built with the money of Wall Street through the use of junk
+# bonds.
+
+# --from Wikipedia.org
+
+# Aria Resort and Casino is a luxury resort and casino, part of the CityCenter
+# complex on the Las Vegas Strip in Paradise, Nevada. Aria consists of two curved
+# glass and steel highrise towers adjoined at the center. It opened on December
+# 16, 2009 as a joint venture between MGM Resorts International and Infinity
+# World Development. At 4,000,000 sq ft (370,000 m2) and 600 ft (180 m) in
+# height, it is the largest and tallest structure at CityCenter.
