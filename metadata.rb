8017daeff0b7f22e5ccadfb511795ce22f74846a
Paul Chicarello <pchicarello@patientslikeme.com>
2016-01-20 11:07:55 -0500

    Update ccokbook versions

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'paul@chicarello.com'
 license 'All rights reserved'
 description 'Configures a node on the lan.chicarello.com domain'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.9'
+version '0.1.16'
 
 depends "chef-client"
 depends "users"
