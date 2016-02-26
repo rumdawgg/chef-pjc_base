20d362915f8d088164a037964c067599583d3f3d
Paul Chicarello <pchicarello@patientslikeme.com>
2016-02-26 14:16:58 -0500

    Manage rsyslog now

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 	'paul@chicarello.com'
 license 			'All rights reserved'
 description 		'Configures a node on the lan.chicarello.com domain'
 long_description 	'Configures a node on the lan.chicarello.com domain'
-version 			'0.1.25'
+version 			'0.1.26'
 
 depends "chef-client"
 depends "users"
@@ -12,3 +12,4 @@ depends "sshd"
 depends "build-essential"
 depends "sudo"
 depends "ssh-keys"
+depends 'rsyslog', '~> 4.0.0'
